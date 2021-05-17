1aa8f52ace577a8c0416d5a3d9986f4a40b9e9a9
Hanna Munoz <hannamunoz@gmail.com>
2021-05-16 23:20:19 -0400

    adding Saved page

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Container } from "../components/Grid/Grid";
+import Nav from "../components/Nav/Nav";
+import Jumbotron from "../components/Jumbotron/Jumbotron";
+import API from "../utils/API";
+import SavedList from "../components/SavedList/SavedList";
+
+
+class Saved extends Component {
+
+    state = {
+        savedBooks: []
+    }
+
+    componentDidMount = () => {
+        this.getBooks()
+    }
+
+    deleteGoogleBook = currentBook => {
+        API.deleteBook( currentBook.id )
+        .then(res => {
+            console.log("This book was deleted", res);
+            this.getBooks();
+        })
+        .catch(err => {
+            console.log("The error", err);
+        })
+    }
+
+
+    getBooks = () => {
+        API.getBooks()
+        .then(res => {
+            this.setState({
+                savedBooks: res.data
+            })
+            console.log("Here is the res from getBooks", res);
+        })
+        .catch(err => {
+            console.log("The error", err);
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Nav />
+                <Container fluid>
+                <Jumbotron />
+                {this.state.savedBooks.length ? (
+                    <SavedList
+                    bookState={this.state.savedBooks}
+                    deleteGoogleBook={this.deleteGoogleBook}
+                    >
+                    </SavedList>
+                ) : (
+                    <h5>No results found</h5>
+                )}
+                </Container>
+            </div>
+        )
+    }
+}
+
+
+export default Saved
