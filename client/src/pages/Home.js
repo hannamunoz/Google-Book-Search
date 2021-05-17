9d3b4d13d632c55ccc9f79e924d1a35f2c5892cc
Hanna Munoz <hannamunoz@gmail.com>
2021-05-17 14:49:05 -0400

    adding bug fixes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,26 +14,32 @@ class Home extends Component {
     };
 
 
-
+    // Create function to search for books through Google API
     searchBooks = () => {
         API.googleBooks(this.state.search)
             .then(res => {
-                console.log("This is the res.data", res.data.items)
+                console.log("This is res.data", res.data.items)
                 this.setState({
-                    books: res.data.items,
-                    search: ""
-                })
-            })
+                books: res.data.items,
+                search: ""
+            })})
             .catch(err => console.log(err));
+            
     };
 
+    // Create function to handle input data
+    handleInputChange = event => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
 
-
+    // Create function to handle form data submission
     handleFormSubmit = event => {
         event.preventDefault();
         this.searchBooks();
     };
-
 
     saveGoogleBook = currentBook => {
         console.log("This is the current book", currentBook);
@@ -45,40 +51,39 @@ class Home extends Component {
             image: currentBook.image,
             link: currentBook.link
         })
-        .then(res => console.log("POST successful to DB", res))
-        .catch(err => console.log("Here is the error", err));
+        .then(res => console.log("Successful POST to DB!", res))
+        .catch(err => console.log("this is the error", err));
     }
-
 
     render() {
         return (
             <div>
                 <Nav />
                 <Container fluid>
-                    <Jumbotron />
-                    <form>
-                        <h5>Search for Books</h5>
-                        <Input
-                            value={this.state.search}
-                            onChange={this.handleInputChange}
-                            name="search"
-                            placeholder="e.g. The Lord of the Rings"
-                        />
-                        <SubmitBtn onClick={this.handleFormSubmit}/>
-                    </form>
-
-                    {this.state.books.length ? (
-                        <ResultList
-                        bookState={this.state.books}
-                        saveGoogleBook={this.saveGoogleBook}>
-                        </ResultList>
-                    ) : (
-                        <div>
-                            <hr/>
-                        <p style={{fontStyle: "italic"}}>No results found</p>
-                        </div>
-                    )}
-
+                <Jumbotron />
+                <form>
+                    <h5>Search for books</h5>
+                    <Input 
+                        value={this.state.search}
+                        onChange={this.handleInputChange}
+                        name="search"
+                        placeholder="e.g. Harry Potter"
+                    />
+                    <SubmitBtn onClick={this.handleFormSubmit}/>
+                </form>
+                
+                {this.state.books.length ? (
+                    <ResultList 
+                    bookState={this.state.books}
+                    saveGoogleBook={this.saveGoogleBook}>
+                    </ResultList>
+                ) : (
+                    <div>
+                        <hr/>
+                    <p style={{fontStyle: "italic"}}>No results to display</p>
+                    </div>
+                )}
+                
                 </Container>
             </div>
         )
